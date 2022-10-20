8a958e3df59567180d790c52b70dca6970c10739
Tristan Haffenden <tristanehaffenden+github@gmail.com>
2022-10-20 11:25:14 +0100

    add function to write version to file

--- internal/files/version_writer.go
+++ internal/files/version_writer.go
@@ -1,6 +1,39 @@
 package files
 
-import "bufio"
+import (
+	"bufio"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// WriteVersionToFile updates the version file with the provided new version
+// value.
+func WriteVersionToFile(dir string, inputFile string, newVersion string) error {
+	versionFunc, err := getVersionFunc(inputFile)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Open(filepath.Clean(filepath.Join(dir, inputFile)))
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalf("error closing file: %s", inputFile)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+
+	if err := versionFunc.writer(scanner, newVersion); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func writeVersionToPackageJSON(scanner *bufio.Scanner, version string) error {
 	return nil
